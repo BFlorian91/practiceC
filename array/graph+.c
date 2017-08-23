c24a3c7528205e0cab0d2c50745e577c65705a0f
Florian <florianbeaumont412@gmail.com>
2017-08-23 16:54:27 +0200

    Update software graph+ add fixe for multiple error

--- array/graph+.c
+++ array/graph+.c
@@ -6,49 +6,56 @@
 /*   By:  <>                                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/08/22 17:39:52 by                   #+#    #+#             */
-/*   Updated: 2017/08/23 00:29:03 by                  ###   ########.fr       */
+/*   Updated: 2017/08/23 16:52:49 by                  ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 
-// Create a SQuarre with modular values
+#define draw printf("A");
 
-/*int		ft_error(int *valueZ)
-{
-	if (*valueZ == 0)
-	{
-		printf("Error, this shit is a SQuarre for you??\n");
-		return (0);
-	}
-	return (0);
-}
-*/
 int		main()
 {
-	int		valueX = 50;
-	int		valueY = 14;
-	
-	
+	int		valueX = 1;
+	int		valueY = 4;
 	int		x;
 	int		y;
 
 	x = 0;
 	y = 0;
-	
+
+/* ---- fix if valueX > 0, valueY is egal valueY + 1 from valueX ---- */
+	if (valueX > 0)
+		valueY -= 1;
+
+/* ---- fix if valueX = 0, valueY display a column ---- */
+	while (valueX == 0)
+	{
+		draw;
+		printf("\n");
+		y++;
+		if (y == valueY)
+			return(0);
+	}
+
+/* ---- main programme ---- */
 	while (x != valueX)
 	{
-		printf("+");
+		draw;
 		x++;
 			if (x == valueX)
 			{
+				if (y == valueY)
+					return(0);
 				printf("\n");
 				x = 0;
 				y++;
 			}
-			if (y == valueY)
-				return(0);
 	}
+
+/* ---- fix if all values = 0, close the software ---- */
+	if (valueX == 0 && valueY == 0)
+		return (0);
 	return(0);
 }
 
